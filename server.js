fa84cb7eea3bf9758762ac74c2126e32b6ac6be4
ichoi21 <ihchoi@gmail.com>
2020-09-16 00:51:24 -0700

    update server

--- server.js
+++ server.js
@@ -3,18 +3,16 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3868;
 
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const publicPath = path.join(__dirname, "..", "public");
-app.use(express.static(publicPath));
+// const publicPath = path.join(__dirname, "..", "public");
+// app.use(express.static(publicPath));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(publicPath, "index.html"));
+// });
 
-
-app.listen(PORT, () => console.log(`Portfolio is up on http://localhost:"${PORT}`));
-
+app.listen(PORT, () =>
+  console.log(`Portfolio is up on http://localhost:"${PORT}`)
+);
